e074686aac87160f4fb27625ddce8d521668df45
Scott Ratigan <scotteratigan@gmail.com>
2019-04-26 19:43:28 -0700

    Updates

---
+++ precache-manifest.14abe0ee029ff09eac24f8f3ccaa2560.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d33623ee31ab03ce5c465498f5aab47",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "d6da091ea1661b1d0dd3",
+    "url": "/hacker-news/static/css/main.c514514d.chunk.css"
+  },
+  {
+    "revision": "1e8e44ecc7f847c6bb1e",
+    "url": "/hacker-news/static/js/2.122ccc6c.chunk.js"
+  },
+  {
+    "revision": "d6da091ea1661b1d0dd3",
+    "url": "/hacker-news/static/js/main.1759a07f.chunk.js"
+  },
+  {
+    "revision": "f4cbfa7cef56548df7d2",
+    "url": "/hacker-news/static/js/runtime~main.393a1947.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.610f49c364ff94d0072080caef1cb6d4.js"
+  "/hacker-news/precache-manifest.14abe0ee029ff09eac24f8f3ccaa2560.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hacker-news/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
